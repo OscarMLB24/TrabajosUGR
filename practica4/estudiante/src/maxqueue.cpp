21dd2dd8f98f5cd05fe270f8c89c8f749ab5c9f6
Óscar Alfonso Mitillo López-Barajas <116158556+OscarMLB24@users.noreply.github.com>
2024-11-11 08:46:30 +0100

    Update maxqueue.cpp

--- practica4/estudiante/src/maxqueue.cpp
+++ practica4/estudiante/src/maxqueue.cpp
@@ -3,3 +3,89 @@
  * @brief  Archivo de implementación del TDA MaxQueue
  * @author
  */
+
+#include <maxqueue.h>
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+ostream& operator<< (ostream& os, Element element) {
+    os << to_string(element.value) << "|" << to_string(element.maximum);
+    return os;
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+MaxQueue::MaxQueue() = default;
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+bool MaxQueue::empty() const
+{
+    return elements.empty();
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+int MaxQueue::size() const
+{
+    return static_cast<int>(elements.size());
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+const Element &MaxQueue::front() const
+{
+    return elements.front();
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+const Element &MaxQueue::back() const
+{
+
+    return elements.back();
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+void MaxQueue::push(int newValue)
+{
+    Element newElement{newValue, newValue};
+    int max = elements.back().maximum;
+
+    if(max > newValue) {
+        newElement.maximum = max;
+    }
+    else{
+        for(Element e : elements) {
+            e.maximum = newValue;
+        }
+    }
+    elements.push_back(newElement);
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+void MaxQueue::pop()
+{
+    elements.pop_front();
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+void MaxQueue::swap(MaxQueue& newQueue)
+{
+    elements.swap(newQueue.elements);
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+ostream &MaxQueue::operator<<(ostream &salida) const
+{
+    for(Element e: elements) {
+        salida << e;
+    }
+    return salida;
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
